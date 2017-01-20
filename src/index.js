445049cc30bbe45a7ceb55787d3730125d2e77fb
muhtarudinsiregar <muhtarudinsiregar93@gmail.com>
2017-01-20 13:23:22 +0700

    class Libur

---
+++ src/index.js
@@ -0,0 +1,31 @@
+/*eslint-env node*/
+/* eslint-disable no-console */
+"use strict";
+const data = require("../hari-libur.json");
+
+class Libur {
+	constructor() { }
+
+	getYears() {
+		var years = data.map((value)=>{
+			return value.year;
+		});
+
+		return years;
+	}
+
+	getDataByYear(year) {
+		var dataLibur = data.filter ((value )=> {
+			if (value.year === year) {
+				return value;
+			}
+			return false;
+		});
+
+		return dataLibur;
+	}
+}
+
+module.exports = function () {
+	return new Libur();
+};
